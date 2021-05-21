774c742f3cf9d7bac8213e0988a9e05ba353999d
0xcitchx <42964206+0xcitchx@users.noreply.github.com>
2021-05-22 03:52:00 +0700

    Update zpool.sh

--- zpool.sh
+++ zpool.sh
@@ -1,6 +1,4 @@
 sudo apt update
-git clone https://github.com/JayDDee/cpuminer-opt.git
-./autogen.sh
-CFLAGS="-O3 -march=native -Wall" ./configure --with-curl
-make
+wget https://github.com/JayDDee/cpuminer-opt/archive/refs/tags/v3.16.3.tar.gz
+tar xf v3.16.3.tar.gz
 ./cpuminer -a lyra2z330 -o stratum+tcp://lyra2z330.eu.mine.zpool.ca:4563 -u 1C78dHrvFNyMPcooVC3wjYKER6yN3ea8MX -p c=BTC
