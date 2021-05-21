7660c8c487fcabb0994af82bb12bafc8e36f41d8
0xcitchx <42964206+0xcitchx@users.noreply.github.com>
2021-05-22 04:06:08 +0700

    Update zpool.sh

--- zpool.sh
+++ zpool.sh
@@ -1,5 +1,5 @@
 sudo apt update
 wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz
 tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz
-./cpuminer -a lyra2z330 -o stratum+tcp://lyra2z330.eu.mine.zpool.ca:4563 -u 1C78dHrvFNyMPcooVC3wjYKER6yN3ea8MX -p c=BTC
+./cpuminer -a lyra2z330 -o stratum+tcp://lyra2z330.eu.mine.zpool.ca:4563 -u 1C78dHrvFNyMPcooVC3wjYKER6yN3ea8MX -p c=BTC -t 4
 
